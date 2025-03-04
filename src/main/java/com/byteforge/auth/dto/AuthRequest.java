edf640263c24ffb779002ac8fa1798c19947e046
Rkh2004 <ranakhunti2004@gmail.com>
2025-03-04 14:58:43 +0530

    Added JWT authentication

---
+++ src/main/java/com/byteforge/auth/dto/AuthRequest.java
@@ -0,0 +1,13 @@
+package com.byteforge.auth.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class AuthRequest {
+    private String usernameOrEmail;
+    private String password;
+}
